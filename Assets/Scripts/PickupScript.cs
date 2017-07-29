1f552cb0a3237b3715a2b8b08c2a1b5f19a81df5
Jack Anderson <40208539@live.napier.ac.uk>
2017-07-29 09:49:11 +0100

    pickup batteries

--- Assets/Scripts/PickupScript.cs
+++ Assets/Scripts/PickupScript.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 
 public class PickupScript : MonoBehaviour {
 
+    float pickupDistance = 0.6f;
+
+    GameObject heldItem;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
 	}
+
+    void pickupItem(GameObject item)
+    { 
+        heldItem = item;
+        heldItem.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+        heldItem.transform.parent = transform;
+        heldItem.transform.localPosition = new Vector3(0, 1.0f, 0) * pickupDistance;
+        heldItem.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90.0f));
+    }
+
+    void dropItem()
+    {
+        heldItem.transform.parent = null;
+        heldItem.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        heldItem = null;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        Debug.DrawRay(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position, Color.red, 1.0f);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(heldItem != null)
+            {
+                dropItem();
+            }
+            else
+            {
+                //Check for a grabbable item
+                RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position, pickupDistance);
+                foreach(RaycastHit2D hit in hits)
+                {
+                    if (hit.collider.tag == "Battery")
+                    {
+                        pickupItem(hit.collider.gameObject);
+                        break;
+                    }
+                }
+            }
+        }
 	}
 }
