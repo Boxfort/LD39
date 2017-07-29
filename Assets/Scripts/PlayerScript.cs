1c8265dded1ae3f890b8e7ad4039f369049b4b12
Jack Anderson <40208539@live.napier.ac.uk>
2017-07-29 08:30:34 +0100

    basic player movement

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    float moveSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Turn player towards the mouse position
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rotation = Quaternion.LookRotation(transform.position - mousePos, Vector3.forward);
+        transform.rotation = rotation;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+
+        //Move player
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+	}
+}
