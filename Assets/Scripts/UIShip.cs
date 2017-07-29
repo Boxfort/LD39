3e51b6975a7d36152993d3c23b871ddec5714c6a
Jack Anderson <40208539@live.napier.ac.uk>
2017-07-29 12:10:02 +0100

    floaty progress ship

---
+++ Assets/Scripts/UIShip.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIShip : MonoBehaviour
+{
+    public Image bar;
+    public Image ship;
+
+    float progress = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float shipX = (Screen.width/20) + (Mathf.Clamp(Screen.width * progress, 0, Screen.width * 0.9f));
+        ship.rectTransform.position = new Vector3(shipX, ship.rectTransform.position.y, ship.rectTransform.position.z);
+
+        progress += 0.2f * Time.deltaTime;
+
+        progress = Mathf.Clamp(progress, 0, 1.0f);
+	}
+}
