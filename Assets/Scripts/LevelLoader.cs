6bdcf6b5ef00d000c05f8addba4542531705967f
Jack Anderson <40208539@live.napier.ac.uk>
2017-07-29 08:12:45 +0100

    very basic level loading

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
 {
-    public Dictionary<Color, GameObject> tileMap = new Dictionary<Color, GameObject>();
+    public Dictionary<Color32, GameObject> tileMap = new Dictionary<Color32, GameObject>();
     public GameObject[] tiles;
 
     public Texture2D map;
@@ -12,30 +12,41 @@ public class LevelLoader : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        //Init Tilemap
-        tileMap.Add(new Color(0, 0, 0), tiles[0]);       //Wall
-        tileMap.Add(new Color(100, 100, 100), tiles[1]); //Floor
-        tileMap.Add(new Color(0, 0, 255), tiles[2]);     //Battery
-        tileMap.Add(new Color(255, 0, 0), tiles[3]);     //Battery port
-        tileMap.Add(new Color(150, 150, 150), tiles[4]); //terminal right
-        tileMap.Add(new Color(155, 150, 150), tiles[5]); //terminal left
-        tileMap.Add(new Color(150, 155, 150), tiles[6]); //terminal up
-        tileMap.Add(new Color(150, 150, 155), tiles[7]); //terminal down
+        Debug.Log("Initialising Tilemap");
+
+        //Init Tilemap 
+        tileMap.Add(new Color32(0, 0, 0, 255), tiles[0]);                //Wall
+        tileMap.Add(new Color32(100, 100, 100, 255), tiles[1]); //Floor
+        tileMap.Add(new Color32(0, 0, 255, 255), tiles[2]);             //Battery
+        tileMap.Add(new Color32(255, 0, 0, 255), tiles[3]);             //Battery port
+        tileMap.Add(new Color32(150, 150, 150, 255), tiles[4]); //terminal right
+        tileMap.Add(new Color32(155, 150, 150, 255), tiles[5]); //terminal left
+        tileMap.Add(new Color32(150, 155, 150, 255), tiles[6]); //terminal up
+        tileMap.Add(new Color32(150, 150, 155, 255), tiles[7]); //terminal down
+
+
+        Debug.Log("Tilemap Initialised");
 
         LoadLevel();
     }
 
     void LoadLevel()
     {
+        Debug.Log("Loading level");
+
+
         //Loop through every pixel on the image supplied
         for (int i = 0; i < map.width; i++)
         {
-            for (int j = 0; j < map.height; i++)
+            for (int j = 0; j < map.height; j++)
             {
-                Color pixel = map.GetPixel(i, j);
+                Color32 pixel = map.GetPixel(i, j);
+
+                Debug.Log("x: " + i + " y: " + j + " | Color = " + pixel.ToString());
+
                 if (tileMap.ContainsKey(pixel))
                 {
-                    Instantiate(tileMap[pixel], new Vector3(i, j, 1.0f), Quaternion.identity);
+                    Instantiate(tileMap[pixel], new Vector3(i, j, 1.0f), tileMap[pixel].transform.rotation);
                 }
             }
         }
